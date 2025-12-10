d90b0e16d443049b4dee56a67e946733011d772c
sdias-code <silviodias.ms@gmail.com>
2025-12-10 15:29:38 -0400

    Implementado ServiceExtensions

---
+++ 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithAspNet10_Scaffold.Data;
+
+namespace RestWithAspNet10_Scaffold.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+    }
+}
--- 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/Program.cs
+++ 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/Program.cs
@@ -1,23 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using RestWithAspNet10_Scaffold.Data;
+using RestWithAspNet10_Scaffold.Extensions;
 using RestWithAspNet10_Scaffold.Services;
 using RestWithAspNet10_Scaffold.Services.Implementations;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<NumberService>();
 builder.Services.AddScoped<IPersonServices, PersonServicesImpl>();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.ConfigureSqlServer(builder.Configuration);
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
