9663846fb5c655edee3b7836ea18a9bfe161e3fc
sdias-code <silviodias.ms@gmail.com>
2026-02-06 14:30:21 -0400

    Implementado DefaultPolicy para Cors

--- 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/Configurations/CorsConfig.cs
+++ 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/Configurations/CorsConfig.cs
@@ -4,6 +4,10 @@
     {
         public static void AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+
+            var origins = configuration.GetSection("Cors:Origins")
+                .Get<string[]>() ?? Array.Empty<string>();
+
             services.AddCors(options =>
              {
                  options.AddPolicy("LocalPolicy", policy =>
@@ -11,6 +15,15 @@
                      policy
                      .WithOrigins("http://localhost:3000", "http://localhost:3001")
                       //.AllowAnyOrigin()
+                     .AllowAnyHeader()
+                     .AllowAnyMethod()
+                     .AllowCredentials();
+                 });
+
+                 options.AddPolicy("DefaultPolicy", policy =>
+                 {
+                     policy
+                     .WithOrigins(origins)             
                      .AllowAnyHeader()
                      .AllowAnyMethod()
                      .AllowCredentials();
