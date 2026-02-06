2257af61aac2a40696129d35754484379f369eb8
sdias-code <silviodias.ms@gmail.com>
2026-02-06 17:32:50 -0400

    Implementado CorsIntegrationTests

---
+++ 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/UnitTestAspNet/RestWithAspNet10/IntegrationTests/CorsIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace RestWithAspNet10.IntegrationTests
+{
+    public class CorsIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+
+        public CorsIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        [Fact]
+        public async Task Should_allow_request_from_allowed_origin()
+        {
+            var allowedOrigins = _configuration
+                .GetSection("Cors:Origins")
+                .Get<string[]>();
+
+            var origin = allowedOrigins!.First();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/v2/person");
+
+            request.Headers.Add("Origin", origin);
+
+            var response = await _client.SendAsync(request);
+
+            Assert.True(
+                response.Headers.Contains("Access-Control-Allow-Origin"));
+
+            var corsHeader = response.Headers
+                .GetValues("Access-Control-Allow-Origin")
+                .First();
+
+            Assert.Equal(origin, corsHeader);
+        }
+
+
+    }
+}
