b90f072a928e5a2ab25199c6a01db700ddec90c2
sdias-code <silviodias.ms@gmail.com>
2026-02-06 17:43:35 -0400

    Implementado class PriorityOrderer

---
+++ 04-RestWithAspNet10-Scaffold/RestWithAspNet10-Scaffold/UnitTestAspNet/RestWithAspNet10/IntegrationTests/Tools/PriorityOrderer.cs
@@ -0,0 +1,36 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace RestWithAspNet10.IntegrationTests.Tools
+{
+    public class PriorityOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>
+            (IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            var sortedMethods = testCases.OrderBy(testCase =>
+            {
+                var priorityAttribute = testCase.TestMethod.Method
+                    .GetCustomAttributes(typeof(TestPriorityAttribute))
+                    .FirstOrDefault();
+                return priorityAttribute != null
+                    ? priorityAttribute.GetNamedArgument<int>("Priority")
+                    : int.MaxValue;
+            });
+
+            return sortedMethods;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public int Priority { get; }
+
+        public TestPriorityAttribute(int priority)
+            => Priority = priority;
+       
+
+
+    }
+}
